c4b02c3fa7f010a56454ce48a5c2140bd1812cc3
wangbiao <wangbiao@corp.rrzcp8.com>
2018-09-08 21:53:53 +0800

    验证了aqs的三种实现

--- concurrent/src/main/java/com/frank/concurrent/aqs/AqsApplication.java
+++ concurrent/src/main/java/com/frank/concurrent/aqs/AqsApplication.java
@@ -1,6 +1,5 @@
 package com.frank.concurrent.aqs;
 
-import sun.nio.ch.ThreadPool;
 
 import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
@@ -15,29 +14,33 @@ public class AqsApplication
 
 	private static Semaphore semaphore = new Semaphore(2, true);
 
+	private CountDownLatch countDownLatch = new CountDownLatch(2);
+
 	private BlockingQueue<Runnable> workQueue = new SynchronousQueue<>();
 
 	ThreadFactory factory = Executors.privilegedThreadFactory();
 
-	private ThreadPoolExecutor executor = new ThreadPoolExecutor(1,1,1000, TimeUnit.SECONDS,
+	private ThreadPoolExecutor executor = new ThreadPoolExecutor(1,5,1000, TimeUnit.SECONDS,
 			workQueue, factory, new ThreadPoolExecutor.AbortPolicy());
 
 	public void singleLock(){
 		try
 		{
-			executor.submit(() -> myJob());
-			executor.submit(() -> myJob());
+			executor.submit(() -> countDownLatch(100));
+			executor.submit(() -> countDownLatch(100));
+			executor.submit(() -> countDownLatch(2000));
+			executor.submit(() -> countDownLatchWait());
 		}catch (Exception e){
 			e.printStackTrace();
 		}
 	}
 
-	private void myJob(){
+	private void reentrantLock(){
 		System.out.println(Thread.currentThread().getName() + " try to get lock");
 		lock.lock();
-		System.out.println(Thread.currentThread().getName() + " get lock");
 		try
 		{
+			System.out.println(Thread.currentThread().getName() + " get lock ############");
 			Thread.sleep(1000);
 		}
 		catch (InterruptedException e)
@@ -47,6 +50,45 @@ public class AqsApplication
 		lock.unlock();
 		System.out.println(Thread.currentThread().getName() + " release lock");
 	}
+
+	private void semaphore(){
+		System.out.println(Thread.currentThread().getName() + " try to get semaphore");
+		try {
+			semaphore.acquire();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(Thread.currentThread().getName() + " get semaphore ############");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		semaphore.release();
+		System.out.println(Thread.currentThread().getName() + " release semaphore");
+	}
+
+	private void countDownLatch(final int time){
+		System.out.println(Thread.currentThread().getName() + " countDownLatch work thread start");
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(Thread.currentThread().getName() + " countDownLatch countDown ############");
+		countDownLatch.countDown();
+	}
+
+	private void countDownLatchWait(){
+		System.out.println(Thread.currentThread().getName() + " countDownLatch wait");
+		try {
+			countDownLatch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(Thread.currentThread().getName() + " countDownLatch wait finished ############");
+	}
+
 
 	public static void main(String[] args){
 		AqsApplication application = new AqsApplication();
